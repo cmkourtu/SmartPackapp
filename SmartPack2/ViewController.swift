6641bce043c2ac9df89225d8d38b8603a7f03314
Craig Kourtu <cmkourtu@gmail.com>
2016-03-24 09:40:12 -0400

    Initial Commit

---
+++ SmartPack2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartPack2
+//
+//  Created by Craig Kourtu on 3/24/16.
+//  Copyright © 2016 ECE477Team1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
