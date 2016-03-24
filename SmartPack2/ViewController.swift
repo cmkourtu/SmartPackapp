f3c19497a5a3fb6395a26758c599159e66bd813a
Craig Kourtu <cmkourtu@gmail.com>
2016-03-24 11:44:39 -0400

    Can connect to private service and read private characteristics, however its reading values as null that are not null.

--- SmartPack2/ViewController.swift
+++ SmartPack2/ViewController.swift
@@ -15,10 +15,25 @@ class ViewController: UIViewController,CBCentralManagerDelegate, CBPeripheralDel
     // BLE
     var centralManager : CBCentralManager!
     var smartpackPeripheral : CBPeripheral!
+    let deviceName = "SMARTPACK2"
     
     // Title labels
     var titleLabel : UILabel!
     var statusLabel : UILabel!
+    
+    // UUID
+    
+    let testConfigUUID1 = CBUUID(string: "12345678-9012-3456-7890-1234567890FF") // UUID for private service
+    let testConfigUUID2 = CBUUID(string: "12345678-9012-3456-7890-123456789AFF") // UUID for private charactersic
+    
+    
+    
+    
+    /******* CBCentralManagerDelegate *******/
+    
+    
+    // The code below controls calls made by the Central Manager
+    
     
     
     func centralManagerDidUpdateState(central: CBCentralManager) {
@@ -39,6 +54,112 @@ class ViewController: UIViewController,CBCentralManagerDelegate, CBPeripheralDel
         }
     }
     
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        
+        
+        
+          // print(advertisementData)
+          // print(nameOfDeviceFound)
+           print(peripheral.name)
+        
+        //   print(peripheral.name)
+        
+        if peripheral.name != nil {
+            let daName = String(peripheral.name!)
+            
+            if daName == deviceName
+            {
+                statusLabel.text = "Found!"
+                print("Got it 2")
+                print(daName)
+                
+                
+                self.smartpackPeripheral = peripheral
+                
+                
+                central.connectPeripheral(peripheral, options: nil)
+                central.stopScan()
+                self.smartpackPeripheral.delegate = self
+              
+                print(peripheral)
+            }
+        }
+
+        
+    }
+    
+    
+    // Discover services of the peripheral
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        self.statusLabel.text = "Discovering peripheral services"
+        peripheral.discoverServices(nil)
+    }
+    
+    
+    // If disconnected, start searching again
+    func centralManager(central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        self.statusLabel.text = "Disconnected"
+        central.scanForPeripheralsWithServices(nil, options: nil)
+    }
+
+    
+    /******* CBCentralPeripheralDelegate *******/
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        self.statusLabel.text = "Looking at peripheral services"
+        for service in peripheral.services! {
+            let thisService = service as CBService
+            if 1 == 1 {        ///// CHANGE THIS LATER TO CHECK FOR VALID SERVICES!!!
+                // Discover characteristics of all valid services
+                peripheral.discoverCharacteristics(nil, forService: thisService)
+            }
+        }
+    }
+    
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+        
+        self.statusLabel.text = "Talking to Bluetooth "
+        
+        
+        
+        for charateristic in service.characteristics! {
+            let thisCharacteristic = charateristic as CBCharacteristic
+            
+            print(thisCharacteristic)
+            print(charateristic)
+     
+            
+            if thisCharacteristic.UUID == testConfigUUID2 {  //// Check for valid characetersic  WARNING
+                // Enable Sensor Notification
+                print("Hello")
+                self.smartpackPeripheral.setNotifyValue(true, forCharacteristic: thisCharacteristic)
+                print(thisCharacteristic)
+            }
+            
+            
+            
+//            if SensorTag.validConfigCharacteristic(thisCharacteristic) {
+//                // Enable Sensor
+//                self.sensorTagPeripheral.writeValue(enablyBytes, forCharacteristic: thisCharacteristic, type: CBCharacteristicWriteType.WithResponse)
+//            }
+        }
+        
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+        
+        self.statusLabel.text = "Connected"
+        
+        print("status")
+        print(characteristic)
+        // self.sensorTagTableView.reloadData()
+    }
+    
+
+    
+    
 
     override func viewDidLoad() {
         print("ViewLoaded")
@@ -57,6 +178,16 @@ class ViewController: UIViewController,CBCentralManagerDelegate, CBPeripheralDel
         titleLabel.sizeToFit()
         titleLabel.center = CGPoint(x: self.view.frame.midX, y: self.titleLabel.bounds.midY+28)
         self.view.addSubview(titleLabel)
+        
+        // Set up status label
+        statusLabel = UILabel()
+        statusLabel.textAlignment = NSTextAlignment.Center
+        statusLabel.text = "Loading..."
+        statusLabel.font = UIFont(name: "HelveticaNeue-Light", size: 12)
+        statusLabel.sizeToFit()
+        //statusLabel.center = CGPoint(x: self.view.frame.midX, y: (titleLabel.frame.maxY + statusLabel.bounds.height/2) )
+        statusLabel.frame = CGRect(x: self.view.frame.origin.x, y: self.titleLabel.frame.maxY, width: self.view.frame.width, height: self.statusLabel.bounds.height)
+        self.view.addSubview(statusLabel)
         
     }
 
